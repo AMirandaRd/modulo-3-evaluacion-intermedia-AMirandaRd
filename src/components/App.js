cbd18d2539db956be6fd02169d5d47d22e9711b7
Amirandard <am.miranda.rd@gmail.com>
2021-09-26 18:08:09 +0200

    fixing some issues

--- src/components/App.js
+++ src/components/App.js
@@ -5,46 +5,44 @@ import { useState } from 'react';
 function App() {
   const [clubs, setClubs] = useState(initialData);
   const [newClubName, setNewClubName] = useState('');
-  const [weekDays, setWeekDays] = useState('')
-  const [weekends, setWeekends]= useState('')
+  const [weekDays, setWeekDays] = useState(false);
+  const [weekends, setWeekends] = useState(false);
 
   const handleNewClubName = (ev) => {
     setNewClubName(ev.target.value);
     console.log(newClubName);
   };
 
-  const handleWeekDays=(ev)=>{
-  setWeekDays(ev.target.value)
-  }
+  const handleWeekDays = (ev) => {
+    setWeekDays(ev.target.checked);
+    console.log(weekDays);
+  };
+  const handleWeekends = (ev) => {
+    setWeekends(ev.target.checked);
+    console.log(weekends);
+  };
 
-  const handleWeekends=(ev)=>{
-    setWeekends(ev.target.value)
-  }
-
-  
-
-  /* const handleClick = (ev) => {
-    ev.preventDefault(); */
-
-  /* const newClub = {
-      "name": newClubName,
-      "openOnWeekdays": openOrnot
-      "openOnWeekends": weekends
-    }; */
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    clubs.push({
+      name: newClubName,
+      weekDays: weekDays,
+      weekends: weekends,
+    });
+    setClubs([...clubs]);
+  };
 
   const renderClubs = () =>
-    initialData.map((club, index) => {
+    clubs.map((club, index) => {
       return (
         <li key={index}>
           <h3>{club.name}</h3>
 
-          <p>
-            Abierto entre semana:{club.openOnWeekdays === true ? 'Sí' : 'No'}
-          </p>
+          <p>Abierto entre semana:{club.openOnWeekdays ? 'Sí' : 'No'}</p>
 
           <p>
             Abierto el fin de semana:Abierto entre semana:
-            {club.openOnWeekend === true ? 'Sí' : 'No'}
+            {club.openOnWeekend ? 'Sí' : 'No'}
           </p>
         </li>
       );
@@ -55,77 +53,40 @@ function App() {
       <h1> Mis clubs</h1>
       <select name='filter' id='filter'>
         Mostrar
+        <option value='all'>Todos</option>
+        <option value='just-weekdays'>los que abren entre semana</option>
+        <option value='just-weekends'>los que abren los fines de semana</option>
       </select>
-      <option value='all'>Todos</option>
-      <option value='just-weekdays'>los que abren entre semana</option>
-      <option value='just-weekends'>los que abren los fines de semana</option>
-
-      {/* <ul>{renderClubs()}</ul> */}
-      <form action=''>
+      {<ul>{renderClubs()}</ul>}
+      <form onSubmit={handleSubmit}>
         <label htmlFor='ClubName'> Nombre del club</label>
         <input
           type='text'
           id='ClubName'
+          value={newClubName}
           placeholder='Introducir un nuevo club'
           onChange={handleNewClubName}
         />
-        <p>Abre entre semana</p>
-        <label htmlFor='openOnWeekdays'>
-          Sí
-         
-        </label>
-        <input
-            id='openOnWeekdays'
-            type='radio'
-            value='yesweekdays'
-            name='weekdays'
-            checked={weekDays === 'yesweekdays'}
-            onChange={handleWeekDays}
-          />
-        <label htmlFor='NotOpenOnWeekdays' value='no-weekdays'>
-          No
-        </label>
-        <input
-            id='NotOpenOnWeekdays'
-            type='radio'
-            value='noweekdays'
-            name='weekdays'
-            checked={weekDays === 'noweekdays'}
-            onChange={handleWeekDays}
-          />
-        <p>Abre los fines de semana</p>
 
-        <label htmlFor='openOnWeekends' value='yes-weekdends'>
-          Sí
-  
-        </label>
+        <label htmlFor=''>¿Abre entre semana?</label>
         <input
-            id='openOnWeekends'
-            type='radio'
-            value='yesweekends'
-            name='weekends'
-            checked={weekends === 'yesweekends'}
-            onChange={handleWeekends}
-          />
+          type='checkbox'
+          name='openOnWeekdays'
+          id='openOnWeekdays'
+          checked={weekDays}
+          onChange={handleWeekDays}
+        />
 
-        <label htmlFor='NotOpenOnWeekends' value='no-weekdends'>
-          No </label>
-          <input
-            id='NotOpenOnWeekends'
-            type='radio'
-            value='noweekends'
-            name='weekends'
-            checked={weekends === 'noweekends'}
-            onChange={handleWeekends}
-          />
-       
+        <label htmlFor=''>¿Abre fines de semana?</label>
         <input
-          className='AddNewClub'
-          type='submit'
-          value='Agregar'
-          /* onclick={handleClick} */
-          Agregar nuevo club
-          />
+          type='checkbox'
+          name='openOnWeekend'
+          id='openOnWeekend'
+          checked={weekends}
+          onChange={handleWeekends}
+        />
+
+        <input type='submit' name='btn' id='btn' value='añadir un nuevo club' />
       </form>
     </>
   );
